47e6b37b89ee93737d09f3312e59556d6e628674
Drummond Biles <biles430@gmail.com>
2017-08-27 11:52:52 -0400

    first upload of code used in PIV analysis

---
+++ piv_readin.py
@@ -0,0 +1,175 @@
+import pandas as pd
+import numpy as np
+import PIV as piv
+import time
+import sys
+import h5py
+from scipy.signal import medfilt
+import matplotlib.pyplot as plt
+import hotwire as hw
+
+################################################################
+#   PURPOSE
+#   1. Readin in PIV data_sets
+#   2. Find Mask
+#   3. Resize
+#   4. Determine mean profiles and HO quantities
+################################################################
+#Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+
+### PIVE READ IN CODE ##############
+####################################
+#list name of data set folders
+base_name = dict()
+#List the base name for each test to be read in and analyzed, names taken directly from folder
+base_name[0] = 'data/Cam_Date=170727_Time=103432_TR_SeqPIV_MP(2x16x16_50ov_ImgCorr)=unknown'
+def piv_readin(date, file_name, base_name, num_images, data_delimiter, sizex, sizey, walloffset, side_error):
+	#Initalize variables
+	num_tests = len(base_name)
+	u = np.ndarray([num_tests, num_images, sizey, sizex])
+	v = np.ndarray([num_tests, num_images, sizey, sizex])
+	v_filt = np.ndarray([num_tests, num_images, sizey, sizex])
+	u_filt = np.ndarray([num_tests, num_images, sizey, sizex])
+	umean = np.ndarray([num_tests, sizey, sizex])
+	#vmean1 = np.ndarray([num_tests, sizey, sizex])
+	#mask = np.zeros([num_tests, 3])
+	umean_profile = dict()
+	vmean_profile = dict()
+	urms_profile = dict()
+	vrms_profile = dict()
+	uvprime_profile = dict()
+	#determine file name
+	file_name = dict()
+	for j in range(1, num_images+1):
+	    file_name[j] = '/AMT00High' + str('{0:02}'.format(j)) + '.dat'
+	for j in base_name:
+	    #Read in
+	    [x, y, u[j], v[j]] = piv.piv_readin_mod(base_name[j], file_name, data_delimiter, num_images+1, sizey, sizex)
+	    #calc u infinity 
+	    u_infinity = np.mean(u[0, 0, -10:-1, 0])
+	    #Filter Images
+	    [u_filt[j], v_filt[j], bad_im_count] = piv.filt_images(u[j], v[j], u_infinity, sizey)
+	    #Obtain mean vector field
+	    umean[j] = np.nanmean(u_filt[j, :], axis=0)
+	    #vmean1[j] = np.nanmean(v_filt[j, :], axis=0)
+
+	#determine mask position
+	tempmask = piv.mask_loc(umean[j])
+	mask = list(tempmask)
+	#use this to find the mean vel in each image, and look for bad images
+
+
+	## Resize vecotor field to crop out masked areas and
+	# create new vectors which take out the masked areas and any side errors
+	sizex_mask = mask[3] - mask[2] - side_error*2
+	sizey_mask = mask[1] - mask[0]
+	Umask = np.ndarray([num_tests, num_images, sizey_mask, sizex_mask])
+	Vmask = np.ndarray([num_tests, num_images, sizey_mask, sizex_mask])
+	umean = np.ndarray([num_tests, sizey_mask, sizex_mask])
+	vmean = np.ndarray([num_tests, sizey_mask, sizex_mask])
+	for j in base_name:
+	    Umask[j] = u_filt[j][:, mask[0]:mask[1], int(mask[2]+side_error):int(mask[3]-side_error)]
+	    Vmask[j] = v_filt[j][:, mask[0]:mask[1], int(mask[2]+side_error):int(mask[3]-side_error)]
+	    umean[j] = np.nanmean(Umask[j], axis=0)
+	    vmean[j] = np.nanmean(Vmask[j], axis=0)
+
+	## Determine RMS quantities ##
+	uprime = np.ndarray([num_tests, num_images, sizey_mask, sizex_mask])
+	vprime = np.ndarray([num_tests, num_images, sizey_mask, sizex_mask])
+	uvprime  = np.ndarray([num_tests, num_images, sizey_mask, sizex_mask])
+	uvprime_mean = np.ndarray([num_tests, sizey_mask, sizex_mask])
+	urms = np.ndarray([num_tests, sizey_mask, sizex_mask])
+	vrms = np.ndarray([num_tests, sizey_mask, sizex_mask])
+	for j in range(0, num_tests):
+		for jj in range(0, num_images):
+			uprime[j, jj] = ((Umask[j][jj]-umean[j]))
+			vprime[j, jj] = ((Vmask[j][jj]-vmean[j]))
+			uvprime[j, jj] = uprime[j, jj]*vprime[j, jj]
+		uvprime_mean[j] = np.nanmean(uvprime[j], axis=0)
+		urms[j] = np.nanmean(uprime[j]**2, axis=0)**(1/2)
+		vrms[j] = np.nanmean(vprime[j]**2, axis=0)**(1/2)
+
+	## wall position adjustment ###########
+	#convert to m and take off wall position as seen in images
+	x = (x)/1000
+	y = (y-walloffset)/1000
+	xmask = x[ (mask[2]+side_error):(mask[3]-side_error) ]
+	ymask = y[ mask[0]:mask[1] ]
+	## Create Mean Profiles for each data set#######
+	for j in range(0, num_tests):
+	    umean_profile[j] = np.mean(umean[j], axis=1)
+	    vmean_profile[j] = np.mean(vmean[j], axis=1)
+	    urms_profile[j] = np.mean(urms[j], axis=1)
+	    vrms_profile[j] = np.mean(vrms[j], axis=1)
+	    uvprime_profile[j] = np.mean(uvprime_mean[j], axis=1)
+
+	## Average multiple profiles together
+	#use this if multiple tests are performed at the same condition
+	umean_profile_avg = np.zeros(len(umean_profile[0]))
+	vmean_profile_avg = np.zeros(len(umean_profile[0]))
+	urms_profile_avg = np.zeros(len(umean_profile[0]))
+	vrms_profile_avg = np.zeros(len(umean_profile[0]))
+	uvprime_profile_avg = np.zeros(len(umean_profile[0]))
+	#average datasets together
+	for j in range(0, num_tests):
+	    umean_profile_avg = umean_profile_avg + umean_profile[j]
+	    vmean_profile_avg = vmean_profile_avg + vmean_profile[j]
+	    urms_profile_avg = urms_profile_avg + urms_profile[j]
+	    vrms_profile_avg = vrms_profile_avg + vrms_profile[j]
+	    uvprime_profile_avg = uvprime_profile_avg + uvprime_profile[j]
+	#divide profiles by number of tests which were combined
+	umean_profile_avg = umean_profile_avg / num_tests
+	vmean_profile_avg = vmean_profile_avg / num_tests
+	urms_profile_avg = urms_profile_avg / num_tests
+	vrms_profile_avg = vrms_profile_avg / num_tests
+	uvprime_profile_avg = uvprime_profile_avg / num_tests
+
+	##calculate conf interval
+	conf = dict()
+	Neff = 75
+	conf['u'] =  (np.nanmean(np.nanmean(np.nanvar(Umask, axis=1), axis=0), axis=1))**(1/2) * (1/Neff)**(1/2)
+	conf['v'] =  (np.nanmean(np.nanmean(np.nanvar(Vmask, axis=1), axis=0), axis=1))**(1/2) * (1/Neff)**(1/2)
+	conf['urms'] =  (np.nanmean(np.nanvar(urms, axis=0), axis=1))**(1/2) * (1/(2*Neff-1))**(1/2)
+	conf['vrms'] =  (np.nanmean(np.nanvar(vrms, axis=0), axis=1))**(1/2) * (1/(2*Neff-1))**(1/2)
+	sigma_u = (np.nanmean(np.nanvar(Umask, axis=1), axis=0))**(1/2)
+	sigma_v = (np.nanmean(np.nanvar(Vmask, axis=1), axis=0))**(1/2)
+	conf['uvprime'] = np.nanmean(sigma_u * sigma_v * (1+ (np.nanmean(uvprime_mean, axis=0)/(sigma_u * sigma_v))**2 / (Neff - 1))**(1/2), axis=1)
+
+	###  WRITE OUT DATA
+	####################
+	#open hdf5 file
+	hdf = pd.HDFStore('data/PIV_' + date + '.h5')
+	hdf.put('umean', pd.DataFrame(umean[0]))
+	hdf.put('vmean', pd.DataFrame(vmean[0]))
+	hdf.put('umean_profile_avg', pd.DataFrame(umean_profile_avg))
+	hdf.put('vmean_profile_avg', pd.DataFrame(vmean_profile_avg))
+	hdf.put('urms_profile_avg', pd.DataFrame(urms_profile_avg))
+	hdf.put('vrms_profile_avg', pd.DataFrame(vrms_profile_avg))
+	hdf.put('uvprime_profile_avg', pd.DataFrame(uvprime_profile_avg))
+	hdf.put('confidence', pd.DataFrame(conf))
+	hdf.put('xaxis', pd.Series(xmask))
+	hdf.put('yaxis', pd.Series(ymask))
+	hdf.put('mask', pd.DataFrame(mask))
+	hdf.close()
+
+	print('Data Saved!')
+	return
